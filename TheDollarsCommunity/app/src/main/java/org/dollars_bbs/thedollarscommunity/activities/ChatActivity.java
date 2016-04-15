f67ec05293e54c9360a02eb382c368f0d9b58d5d
Emmanuel <emmanuelbendavid@gmail.com>
2016-04-15 13:39:39 -0300

    Updated README

--- TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/ChatActivity.java
+++ TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/ChatActivity.java
@@ -32,7 +32,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.gson.annotations.SerializedName;
