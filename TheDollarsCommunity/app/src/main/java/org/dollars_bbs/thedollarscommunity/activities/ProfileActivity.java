b01fbfcef77272dc65b1d7f541e341ff74f1c559
Emmanuel <emmanuelbendavid@gmail.com>
2016-05-07 20:54:46 -0300

    Converted to Lambda

--- TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/ProfileActivity.java
+++ TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/ProfileActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import org.dollars_bbs.thedollarscommunity.R;
 
@@ -22,12 +21,9 @@ public class ProfileActivity extends AppCompatActivity {
 
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		assert fab != null;
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				// TODO: 2016-03-30 private chat
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-			}
+		fab.setOnClickListener(view->{
+			// TODO: 2016-03-30 private chat
+			Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
 		});
 	}
 
--- TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/SettingsActivity.java
+++ TheDollarsCommunity/app/src/main/java/org/dollars_bbs/thedollarscommunity/activities/SettingsActivity.java
@@ -5,9 +5,6 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -16,8 +13,6 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
 
 import org.dollars_bbs.thedollarscommunity.R;
@@ -129,27 +124,24 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
 	 * A preference value change listener that updates the preference's summary
 	 * to reflect its new value.
 	 */
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = (preference, value)->{
+		String stringValue = value.toString();
 
-			if (preference instanceof ListPreference) {
-				// For list preferences, look up the correct display value in
-				// the preference's 'entries' list.
-				ListPreference listPreference = (ListPreference) preference;
-				int index = listPreference.findIndexOfValue(stringValue);
+		if (preference instanceof ListPreference) {
+			// For list preferences, look up the correct display value in
+			// the preference's 'entries' list.
+			ListPreference listPreference = (ListPreference) preference;
+			int index = listPreference.findIndexOfValue(stringValue);
 
-				// Set the summary to reflect the new value.
-				preference.setSummary(index >= 0? listPreference.getEntries()[index]:null);
+			// Set the summary to reflect the new value.
+			preference.setSummary(index >= 0? listPreference.getEntries()[index]:null);
 
-			} else {
-				// For all other preferences, set the summary to the value's
-				// simple string representation.
-				preference.setSummary(stringValue);
-			}
-			return true;
+		} else {
+			// For all other preferences, set the summary to the value's
+			// simple string representation.
+			preference.setSummary(stringValue);
 		}
+		return true;
 	};
 
 	/**
