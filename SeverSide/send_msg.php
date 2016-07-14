a6a3963494a58c710363e745e211aad149e73bbc
Emmanuel <emmanuelbendavid@gmail.com>
2016-07-13 23:57:40 -0300

    Generalized

--- SeverSide/send_msg.php
+++ SeverSide/send_msg.php
@@ -6,30 +6,25 @@
 		$nick = $_POST['nick'];
 
 		if(isset($_POST['is_text']) && $_POST['is_text']) {
-			if(isset($_POST['msg'])){
-				$msg = $_POST['msg'];
-
-				if($chat === "global") {
-					$query = "INSERT INTO $global_chat_table (time, nick, isImage, msg) VALUES ($time, '$nick', 0, '$msg')";
-				} elseif(isset($_POST['receiver'])) {
-					$query = "INSERT INTO $private_chat_table (time, id_sender, id_receiver, isImage, msg) VALUES ($time, '$nick', 0, '$msg')";
-				}
-			}
+			$msg = $_POST['msg'];
 		} elseif(isset($_POST['image'])) {
 			$msg = $_POST['image'];
-
-			if($chat == "global")
-				$query = "INSERT INTO $global_chat_table (time, nick, isImage, msg) VALUES($time, '$nick', 1, $image)";
-			elseif(isset($_POST['receiver'])) {
-				$query = "INSERT INTO $private_chat_table (time, id_sender, id_receiver, isImage, msg) VALUES ($time, '$nick', 1, '$image')";
+		}
+		
+		if(isset($_POST['msg'])){
+			if($chat === "global") {
+				$query = "INSERT INTO $global_chat_table (time, nick, isImage, msg) VALUES ($time, '$nick', 0, '$msg')";
+			} elseif(isset($_POST['receiver'])) {
+				$query = "INSERT INTO $private_chat_table (time, id_sender, id_receiver, isImage, msg) VALUES ($time, '$nick', 0, '$msg')";
 			}
 		}
 
 		$result = mysqli_query($conn, $query);
 		
 		if(!$result) die(mysqli_error($conn).". Query: '$query'.");
-		
-		echo(($result?"succes":"failure"));
 	}
+
+	echo(($result?"succes":"failure"));
+
 	include_once("return.php"); 
 ?>
