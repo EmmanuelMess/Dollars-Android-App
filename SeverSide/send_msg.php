8514b856e29916f646a6a9fb79117977c08ac96c
Emmanuel <emmanuelbendavid@gmail.com>
2016-07-28 23:31:29 -0300

    Added error when msg fails and deleted time from post

--- SeverSide/send_msg.php
+++ SeverSide/send_msg.php
@@ -6,26 +6,26 @@
 		$nickId = $_POST['nick'];
 
 		if(isset($_POST['is_text']) && $_POST['is_text']) {
+			$isImage = 0;
 			$msg = $_POST['msg'];
-		} elseif(isset($_POST['image'])) {
+		} else {
+			$isImage = 1;
 			$msg = $_POST['image'];
 		}
 		
 		if(isset($_POST['msg'])){
 			if($chat === "global") {
-				$query = "INSERT INTO $global_chat_table (time, nick, isImage, msg) VALUES ($time, $nickId, 0, '$msg')";
+				$query = "INSERT INTO $global_chat_table (time, nick, isImage, msg) VALUES ($time, $nickId, $isImage, '$msg')";
 			} elseif(isset($_POST['receiver'])) {
 				$receiverId = $_POST['receiver'];
-				$query = "INSERT INTO $private_chat_table (time, id_sender, id_receiver, isImage, msg) VALUES ($time, $nickId, $receiverId, 0, '$msg')";
+				$query = "INSERT INTO $private_chat_table (time, id_sender, id_receiver, isImage, msg) VALUES ($time, $nickId, $receiverId, $isImage, '$msg')";
 			}
 		}
 
-		$result = mysqli_query($conn, $query);
-		
-		if(!$result) die(mysqli_error($conn).". Query: '$query'.");
+		$result = mysqli_query($conn, $query);		
 	}
 
-	echo(($result?"succes":"failure"));
+	echo(($result?"success":die(mysqli_error($conn).". Query: '$query'.")));
 
 	include_once("return.php"); 
 ?>
