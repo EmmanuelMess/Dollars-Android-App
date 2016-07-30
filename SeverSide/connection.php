1c61049076f3096eea6f2106b7404aa664f48991
Emmanuel <emmanuelbendavid@gmail.com>
2016-07-29 21:24:07 -0300

    Added " ' " to querries and optimized phps

--- SeverSide/connection.php
+++ SeverSide/connection.php
@@ -1,5 +1,5 @@
 <?php
-	$ver = "4";
+	$ver = "5";
 	$server = "";//replaced it with database server name
 	$username = "";//replaced it with database username
 	$password = "";//replaced it with database password
